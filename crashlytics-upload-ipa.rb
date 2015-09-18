662e2e3b8a5ad21d07824bf87627ba5b49d27d52
Nicolae Ghimbovschi <xfreebird@gmail.com>
2015-09-18 19:11:54 +0300

    Added crashlytics-upload-ipa

---
+++ crashlytics-upload-ipa.rb
@@ -0,0 +1,16 @@
+class CrashlyticsUploadIPA < Formula
+  desc "Upload an IPA to Crashlytics without Crashlytics.framework"
+  homepage "https://github.com/xfreebird/crashlytics-upload-ipa"
+  url "https://github.com/xfreebird/crashlytics-upload-ipa/archive/1.0.0.tar.gz"
+  sha256 "d72ec979aca7b448226a9fcb01850d0d98a9c7ffdd191294b881682adccd4547"
+  version "1.0.0"
+
+  def install
+    bin.install "crashlytics-submit"
+    bin.install "crashlytics-upload-ipa"
+  end
+
+  test do
+    system "crashlytics-upload-ipa", "--version"
+  end
+end
