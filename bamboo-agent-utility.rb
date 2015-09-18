27e49cc4e6a8862edfd4ec16d7900f5c81f33f3c
Nicolae Ghimbovschi <xfreebird@gmail.com>
2015-09-18 16:01:33 +0300

    updated bamboo-agent-utility

--- bamboo-agent-utility.rb
+++ bamboo-agent-utility.rb
@@ -2,7 +2,7 @@ class BambooAgentUtility < Formula
   desc "A Bamboo CI utility for installing or removing an agent on OS X"
   homepage "https://github.com/xfreebird/bamboo-agent-utility"
   url "https://github.com/xfreebird/bamboo-agent-utility/archive/1.0.0.tar.gz"
-  sha256 "3f382bb8a7aa725257762a170d4d7566ab1925f83b97e01baec7e3ccf8b413f9"
+  sha256 "8e5b5ad5f03395a5d31cf115dc9c2736a532af8c5a31b0df0acf89676d8a4ab8"
   version "1.0.0"
 
   def install
