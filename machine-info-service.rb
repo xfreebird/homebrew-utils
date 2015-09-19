0e898a62e8e19291f768fae07f56633530849ed7
Nicolae Ghimbovschi <xfreebird@gmail.com>
2015-09-20 00:27:38 +0300

    Upgraded machine-info-service

--- machine-info-service.rb
+++ machine-info-service.rb
@@ -1,18 +1,18 @@
 class MachineInfoService < Formula
   desc "OS X build machine info service"
   homepage "https://github.com/xfreebird/osx-build-machine-info-service"
-  url "https://github.com/xfreebird/osx-build-machine-info-service/archive/1.0.3.tar.gz"
-  sha256 "9f63d120487a67a57d1f264d5236943ccf447fa47cf38aef5262ea4f863fc307"
-  version "1.0.3"
+  url "https://github.com/xfreebird/osx-build-machine-info-service/archive/1.0.4.tar.gz"
+  sha256 "39aef7516ec4e55691fcd71aeebaae646c967036d5318eb04adf35da4c110c1c"
+  version "1.0.4"
 
   depends_on "node"
 
   def install
-    bin.install "info-service-helper"
-    bin.install "machine-info"
-    share.install "httpd.conf"
-    share.install "index.html"
-    libexec.install "websocketd.js"
+    bin.install "bin/info-service-helper"
+    bin.install "bin/machine-info"
+    share.install "share/httpd.conf"
+    share.install "share/index.html"
+    libexec.install "libexec/websocketd.js"
   end
 
   test do
