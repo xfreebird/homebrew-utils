c88d4402d5b407b556521b270827784dec4f76f3
Nicolae Ghimbovschi <xfreebird@gmail.com>
2015-09-19 23:22:07 +0300

    Fixed machine-info-service

--- machine-info-service.rb
+++ machine-info-service.rb
@@ -1,14 +1,18 @@
 class MachineInfoService < Formula
   desc "OS X build machine info service"
   homepage "https://github.com/xfreebird/osx-build-machine-info-service"
-  url "https://github.com/xfreebird/osx-build-machine-info-service/archive/1.0.0.tar.gz"
-  sha256 "c3149cc0859b9f1acad3b0b011466a0917113908086b8ceffbf43276b7182432"
-  version "1.0.0"
+  url "https://github.com/xfreebird/osx-build-machine-info-service/archive/1.0.1.tar.gz"
+  sha256 "cb0183934e3dab81dec5465f04a1422e65aac4b78744ee2385afd428552c6f24"
+  version "1.0.1"
+
+  depends_on "node"
 
   def install
-    libexec.install Dir["*"]
     bin.install "info-service-helper"
     bin.install "machine-info"
+    share.install "httpd.conf"
+    share.install "index.html"
+    libexec.install "websocketd.js"
   end
 
   test do
