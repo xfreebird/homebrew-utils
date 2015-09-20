0dea4f77b152c8b2c6f44741836f86a0e1775df0
Nicolae Ghimbovschi <xfreebird@gmail.com>
2015-09-20 18:40:27 +0300

    Added iosbuilder

---
+++ iosbuilder.rb
@@ -0,0 +1,31 @@
+class IosBuilder < Formula
+  desc "A script for building, testing and analyzing iOS projects"
+  homepage "https://github.com/xfreebird/iosbuilder"
+  url "https://github.com/xfreebird/iosbuilder/archive/2.0.1.tar.gz"
+  sha256 "3605c59a51e4cebd91a54509b025a402c514ceb6e10e3584686a04657afb1bae"
+  version "2.0.1"
+
+  depends_on "xctool"
+  depends_on "gcovr"
+  depends_on "caskroom/cask/brew-cask"
+  depends_on "ocunit2junit" => :ruby
+  depends_on "xcpretty" => :ruby
+  depends_on "cocoapods" => :ruby
+  depends_on :xcode
+
+  def install
+    system "ln", "-s", "iosbuilder.sh", "iosbuilder"
+    bin.install "iosbuilder.sh"
+    bin.install "iosbuilder"
+    system "brew", "cask", "install", "oclint"
+  end
+
+  test do
+    system "iosbuilder.sh", "--version"
+  end
+
+  def caveats; <<-EOS.undent
+    caveats
+    EOS
+  end
+end
