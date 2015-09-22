46b4f658dc287447612bb778c0292b16442f1074
Nicolae Ghimbovschi <xfreebird@gmail.com>
2015-09-22 20:39:01 +0300

    Updated splunk-mobile-upload

--- splunk-mobile-upload.rb
+++ splunk-mobile-upload.rb
@@ -1,9 +1,9 @@
 class SplunkMobileUpload < Formula
   desc "Upload iOS dSYM or Proguard mapping to Splunk MINT"
   homepage "https://github.com/xfreebird/splunk-mobile-upload"
-  url "https://github.com/xfreebird/splunk-mobile-upload/archive/1.0.0.tar.gz"
-  sha256 "161da0e2ec2181250490c0d9336948df3c15bc61feab6bf3a6c6b79fe3828b6e"
-  version "1.0.0"
+  url "https://github.com/xfreebird/splunk-mobile-upload/archive/1.0.1.tar.gz"
+  sha256 "adee43a6b8671a93e6ed8604d82e00677760abaab59531987779d67ff3dc48ed"
+  version "1.0.1"
 
   def install
     bin.install "splunk-mobile-upload"
