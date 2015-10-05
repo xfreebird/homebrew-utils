fd71e2f39d431adcce315500e15194a4e89410e3
Nicolae Ghimbovschi <xfreebird@gmail.com>
2015-10-05 10:45:23 +0300

    Update iosbuilder.rb
    Removed dependencies

--- iosbuilder.rb
+++ iosbuilder.rb
@@ -4,17 +4,13 @@ class Iosbuilder < Formula
   url "https://github.com/xfreebird/iosbuilder/archive/2.0.3.zip"
   sha256 "4d88d649ed5a316d6bd7e50127f4f51bd4afc9f01d7eddc430690a21b0cb35df"
   version "2.0.3"
-
-  depends_on "xctool"
-  depends_on "gcovr"
-  depends_on "caskroom/cask/brew-cask"
+  
   depends_on :xcode
 
   def install
     FileUtils.ln_s "iosbuilder.sh", "iosbuilder"
     bin.install "iosbuilder.sh"
     bin.install "iosbuilder"
-    system "brew-cask", "install", "oclint"
 
     user = ENV['USER']
     keychain = "/Users/#{user}/Library/Keychains/iosbuilder.keychain"
@@ -32,6 +28,7 @@ class Iosbuilder < Formula
   def caveats; <<-EOS.undent
     Please install ocunit2junit, xcpretty and cocoapods:
     sudo gem install ocunit2junit xcpretty cocoapods
+    Also oclint and xctool.
     The iosbuilder.keychain is located at ~/Library/Keychains/iosbuilder.keychain.
     And it has a blank password (e.g. it has no password)
     EOS
