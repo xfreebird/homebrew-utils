1a7911387818f96248378e0c6ef685bd0e125148
Nicolae Ghimbovschi <xfreebird@gmail.com>
2015-10-14 21:09:55 +0300

    Updated refresh-ios-profiles formula

--- refresh-ios-profiles.rb
+++ refresh-ios-profiles.rb
@@ -1,9 +1,9 @@
 class RefreshIosProfiles < Formula
   desc "Downloads and installs all iOS provisioning profiles from Apple Developer Portal."
   homepage "https://github.com/xfreebird/refresh-ios-profiles"
-  url "https://github.com/xfreebird/refresh-ios-profiles/archive/1.0.2.tar.gz"
-  sha256 "cdd3b736e16242b55f1e98d292646ee7fb8ee481e9c47e88576bbc216aae7141"
-  version "1.0.2"
+  url "https://github.com/xfreebird/refresh-ios-profiles/archive/1.0.3.tar.gz"
+  sha256 "158c3b48af31d8d897ec6fd41b905ccf293f89ecb1886f346360e2551316bc70"
+  version "1.0.3"
 
   def install
     bin.install "refresh-ios-profiles"
