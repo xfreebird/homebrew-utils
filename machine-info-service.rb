5333af532d67052ce7890c7e0ace8e5e9e3b3793
Nicolae Ghimbovschi <xfreebird@gmail.com>
2015-10-23 00:05:10 +0300

    Updated OSX info script

--- machine-info-service.rb
+++ machine-info-service.rb
@@ -1,9 +1,9 @@
 class MachineInfoService < Formula
   desc "OS X build machine info service"
   homepage "https://github.com/xfreebird/osx-build-machine-info-service"
-  url "https://github.com/xfreebird/osx-build-machine-info-service/archive/1.0.4.tar.gz"
-  sha256 "39aef7516ec4e55691fcd71aeebaae646c967036d5318eb04adf35da4c110c1c"
-  version "1.0.4"
+  url "https://github.com/xfreebird/osx-build-machine-info-service/archive/1.0.5.tar.gz"
+  sha256 "17fa510ff24448c2f4ff387d8de4d27f4ca9369aae60bba5d7dcd9b0cdca8618"
+  version "1.0.5"
 
   depends_on "node"
 
