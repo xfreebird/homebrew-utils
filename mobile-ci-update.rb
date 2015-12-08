5bdcc6eff2430fed413270fb97af92744eb754fc
Nicolae Ghimbovschi <xfreebird@gmail.com>
2015-12-08 23:49:56 +0200

    Updated forumlas

--- mobile-ci-update.rb
+++ mobile-ci-update.rb
@@ -1,9 +1,9 @@
 class MobileCiUpdate < Formula
   desc "Upgrade your's Mobile CI machine software"
   homepage "https://github.com/xfreebird/mobile-ci-bootstrap"
-  url "https://github.com/xfreebird/mobile-ci-bootstrap/archive/1.0.10.zip"
-  sha256 "626b95c7620d4778f180de4e13bee23fcaeaa8b5e62bbe162e5650f38d3a9aa0"
-  version "1.0.10"
+  url "https://github.com/xfreebird/mobile-ci-bootstrap/archive/1.0.12.zip"
+  sha256 "b9d26ac85c9fe7475fb06df486fe50a6fb6316b0d6c7c72048540787b2f71cc2"
+  version "1.0.12"
 
   def install
     FileUtils.ln_s "mobile-ci-update.sh", "mobile-ci-update"
--- mobile-dev-update.rb
+++ mobile-dev-update.rb
@@ -1,9 +1,9 @@
 class MobileDevUpdate < Formula
   desc "Upgrade your's Mobile Dev machine software"
   homepage "https://github.com/xfreebird/mobile-dev-bootstrap"
-  url "https://github.com/xfreebird/mobile-dev-bootstrap/archive/1.0.10.zip"
-  sha256 "f6a061768ec69856d59d67e1e820a3edb2812cf28eb7eadb1f71d193134f3098"
-  version "1.0.10"
+  url "https://github.com/xfreebird/mobile-dev-bootstrap/archive/1.0.11.zip"
+  sha256 "246c17a937ffc2476a22218678a62f771403df68b586999dafea663e2ea76654"
+  version "1.0.11"
 
   def install
     FileUtils.ln_s "mobile-dev-update.sh", "mobile-dev-update"
