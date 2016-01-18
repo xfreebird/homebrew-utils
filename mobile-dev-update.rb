615b5e55e08b93a8c0b7112d1b92995a40f9360b
Nicolae Ghimbovschi <Nicolae.Ghimbovschi@endava.com>
2016-01-18 12:57:50 +0200

    Update mobile-ci/dev

--- mobile-dev-update.rb
+++ mobile-dev-update.rb
@@ -2,7 +2,7 @@ class MobileDevUpdate < Formula
   desc "Upgrade your's Mobile Dev machine software"
   homepage "https://github.com/xfreebird/mobile-dev-bootstrap"
   url "https://github.com/xfreebird/mobile-dev-bootstrap/archive/1.0.14.zip"
-  sha256 "300cc26ded036bb8b6d90f27a6b5eb8f1b593ca63a23610f133e79977a45447d"
+  sha256 "7fdca308a2e87a75f392233e25fe7b651cc26209efc185094dccce0450391180"
   version "1.0.14"
 
   def install
