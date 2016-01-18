586b8cebe17b23b1f6d250ef062f0fa45c16268c
Nicolae Ghimbovschi <Nicolae.Ghimbovschi@endava.com>
2016-01-18 12:54:11 +0200

    Update mobile-ci/dev

--- mobile-ci-update.rb
+++ mobile-ci-update.rb
@@ -2,7 +2,7 @@ class MobileCiUpdate < Formula
   desc "Upgrade your's Mobile CI machine software"
   homepage "https://github.com/xfreebird/mobile-ci-bootstrap"
   url "https://github.com/xfreebird/mobile-ci-bootstrap/archive/1.0.14.zip"
-  sha256 "5c965e69bdd0c9963ddd6004da269b80aa99e68c7b266b09e5b8ca026667435a"
+  sha256 "74fb43f1e62d87ec60e14056f12a5d1949ef281c368c7dec2b49559e4aee0bc5"
   version "1.0.14"
 
   def install
