a938e70bba543f6bcdb1409c00bf45a7cbb31a82
Nicolae Ghimbovschi <nicolae.ghimbovschi@travelportdigital.com>
2018-04-17 23:40:51 +0300

    Upgrade mobile-ci

--- mobile-ci-update.rb
+++ mobile-ci-update.rb
@@ -1,9 +1,9 @@
 class MobileCiUpdate < Formula
   desc "Upgrade your's Mobile CI machine software"
   homepage "https://github.com/xfreebird/mobile-ci-bootstrap"
-  url "https://github.com/xfreebird/mobile-ci-bootstrap/archive/2.0.0.zip"
-  sha256 "ef11eabc2f5084f89c380bdd5020bfcdcdde6f3421ca128bbf6c3455589fc61a"
-  version "2.0.0"
+  url "https://github.com/xfreebird/mobile-ci-bootstrap/archive/2.0.1.zip"
+  sha256 "977449a92cdc9934d07319d38945a71f5ceb0cd474dc5080ae5cb2774f7a5a6f"
+  version "2.0.1"
 
   def install
     FileUtils.ln_s "mobile-ci-update.sh", "mobile-ci-update"
