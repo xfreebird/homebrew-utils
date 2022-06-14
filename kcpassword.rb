f5fe4ecead61d4501b46d2e1f501300c2362a4d9
Nicolae Ghimbovschi <nicolae.ghimbovschi@travelportdigital.com>
2022-06-14 14:30:55 +0300

    Update kcpassword formula

--- kcpassword.rb
+++ kcpassword.rb
@@ -1,9 +1,9 @@
 class Kcpassword < Formula
   desc "Enable OS X autologin"
   homepage "https://github.com/xfreebird/kcpassword"
-  url "https://github.com/xfreebird/kcpassword/archive/1.0.0.tar.gz"
-  sha256 "611620edd25f93b2f118f08ca60d1a7c6ed55f86195bab41da9f2011aae854f6"
-  version "1.0.0"
+  url "https://github.com/xfreebird/kcpassword/archive/1.1.0.tar.gz"
+  sha256 "318afc8f1dadf4bcce60b7f83dc4009d37ec45124c04cb0346c762aaa1b2d6d5"
+  version "1.1.0"
 
   def install
     bin.install "kcpassword"
